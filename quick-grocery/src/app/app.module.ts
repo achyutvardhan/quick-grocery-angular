fd80e5b8a1c71b3816c68b61e2abf361987125fc
achyutvardhan <acvardhan@deloitte.com>
2025-06-11 10:21:57 +0530

    product section

--- quick-grocery/src/app/app.module.ts
+++ quick-grocery/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from './sub-component/product-card/product-card.component';
+import { OurProductSectionComponent } from   './our-product-section/our-product-section.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCardComponent,
+    OurProductSectionComponent
   ],
   imports: [
     BrowserModule
