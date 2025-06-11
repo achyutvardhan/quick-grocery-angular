0e5fe5b7d986927afd6f315cda4fa2a8dbf26925
achyutvardhan <acvardhan@deloitte.com>
2025-06-11 10:07:32 +0530

    Revert "product section"
    This reverts commit 5a3a9b14b57e688edbfb2e8129321533ebd370a9.

--- quick-grocery/src/app/app.module.ts
+++ quick-grocery/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductCardComponent } from './sub-component/product-card/product-card.component';
-import { OurProductSectionComponent } from   './our-product-section/our-product-section.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductCardComponent,
-    OurProductSectionComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
