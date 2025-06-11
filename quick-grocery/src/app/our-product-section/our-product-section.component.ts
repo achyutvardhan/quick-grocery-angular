5a3a9b14b57e688edbfb2e8129321533ebd370a9
achyutvardhan <acvardhan@deloitte.com>
2025-06-11 09:53:38 +0530

    product section

---
+++ quick-grocery/src/app/our-product-section/our-product-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-product-section',
+  templateUrl: './our-product-section.component.html',
+  styleUrls: ['./our-product-section.component.css']
+})
+export class OurProductSectionComponent {
+
+}
