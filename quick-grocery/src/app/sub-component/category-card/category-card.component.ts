880ab57660ffacec208bda6979078054f8aa6750
gaurav <gnahata@deloitte.com>
2025-06-12 21:45:36 +0530

    updated top-category

--- quick-grocery/src/app/sub-component/category-card/category-card.component.ts
+++ quick-grocery/src/app/sub-component/category-card/category-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-category-card',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-card.component.css']
 })
 export class CategoryCardComponent {
+  @Input() itemsObj2: any;
 
+  constructor(){
+    this.itemsObj2 = {
+      image:'',
+      name:'',
+      price:0,
+      category:''
+    };
+  }
 }
