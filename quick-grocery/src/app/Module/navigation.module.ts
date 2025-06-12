e87a41ba3f1b96bd65a27be4840162f1c33eb21b
Piyush Ranjan Satapathy <109914906+Alfa-Satapathy@users.noreply.github.com>
2025-06-12 18:16:59 +0530

    add to cart updated

--- quick-grocery/src/app/Module/navigation.module.ts
+++ quick-grocery/src/app/Module/navigation.module.ts
@@ -7,6 +7,8 @@ import { ContactComponent } from '../Pages/contact/contact.component';
 import { SpecialOfferComponent } from '../Pages/special-offer/special-offer.component';
 import { TopCategoryComponent } from '../top-category/top-category.component';
 import { OurProductComponent } from '../Pages/our-product/our-product.component';
+import { AddToCartComponent } from '../Pages/add-to-cart/add-to-cart.component';
+
 export const appRoutes: Routes = [
   { path: "", component: HomeComponent },
   {path: "ourProduct" , component:OurProductComponent},
@@ -14,7 +16,9 @@ export const appRoutes: Routes = [
   {path: "contact" , component: ContactComponent},
   {path: "specialOffer" , component: SpecialOfferComponent},
   {path:"topCategory" , component:TopCategoryComponent},
+  {path: 'add-to-cart', component:AddToCartComponent}
 ];
+
 @NgModule({
   declarations: [],
   imports: [
