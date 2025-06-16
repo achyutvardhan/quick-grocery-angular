2440ad763061b72d6f24b98c22e343af8bdf97b8
achyutvardhan <acvardhan@deloitte.com>
2025-06-16 11:51:24 +0530

    cart item display on header

--- quick-grocery/src/app/app.component.ts
+++ quick-grocery/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { CartServiceService } from './services/cart-service.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quick-grocery';
+  private cartItems: any[] = [];
+  totalQuantity: number = 0;
+  private cartSubscription: Subscription | undefined;
+  
+  constructor(private cartService: CartServiceService) {
+    this.cartSubscription = this.cartService.cartItems$.subscribe((data)=>
+        {
+          this.cartItems = Array.isArray(data) ? data : [];
+          this.calculateTotalQuantity();
+        }
+      );
+  }
+
+  calculateTotalQuantity(): void {
+    this.totalQuantity = this.cartItems.reduce((acc, item) => acc + item.quantity, 0);
+  }
+       
 }
